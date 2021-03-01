0916c6aa2912eb7847f3e378ced033fa635e2074
alexnorberto <alexsandronorberto@gmail.com>
2021-03-01 13:29:43 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { Task } from './components/task/task';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent, TaskDialogResult } from './components/task-dialog/task-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kanban-fire';
+
+  todo: Task[] = [
+    { title: "task1", description:"description task1"},
+    { title: "task2", description:"description task2"},
+    { title: "task2", description:"description task2"}
+  ];
+  inProgress: Task[]=[];
+  done: Task[]=[];
+
+  constructor(
+    private dialog:MatDialog
+  ){
+
+  }
+
+
+
+  drop(event: CdkDragDrop<Task[]>): void{
+    if (event.previousContainer == event.container){
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    )
+  }
+
+  edit(list: 'done' | 'todo' | 'inProgress',task:Task):void{
+    const dialogRef = this.dialog.open(TaskDialogComponent,{
+      width:'270px',
+      data:{
+        task,
+        enableDelete: true
+      }
+    });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
+      const dataList = this[list];
+      const taskIndex = dataList.indexOf(task);
+      if (result.delete) {
+        dataList.splice(taskIndex,1);
+      } else {
+        dataList[taskIndex] = task;
+      }
+    });
+  
+  }
+
+  newTask(){
+    const dialogRef = this.dialog.open(TaskDialogComponent,{
+      width:'270px',
+      data:{
+        task:{}
+      }
+    });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) =>
+      this.todo.push(result.task))
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule }   from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TaskComponent } from './components/task/task.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatDialogModule} from '@angular/material/dialog';
+import { TaskDialogComponent } from './components/task-dialog/task-dialog.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TaskDialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule,
+    FormsModule,
+    MatToolbarModule, MatIconModule, MatCardModule, MatButtonModule, BrowserAnimationsModule, MatDialogModule,
+    MatFormFieldModule, MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
