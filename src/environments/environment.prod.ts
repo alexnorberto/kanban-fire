34abfdd58731c292f9879a7f56165626c1087d24
alexnorberto <alexsandronorberto@gmail.com>
2021-03-01 14:55:33 -0300

    start fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyA7Bxwvyf0EGeZo9PiWPHOMK0jIZUARSPc",
+    authDomain: "kanban-fire-1dc60.firebaseapp.com",
+    projectId: "kanban-fire-1dc60",
+    storageBucket: "kanban-fire-1dc60.appspot.com",
+    messagingSenderId: "408821650699",
+    appId: "1:408821650699:web:e9b775c6a978eddaa37e0e",
+    measurementId: "G-CRXZM9ZTV0"
+  }
 };
